7183c0dd7c5f352c6f195256b9e21922dc554ce9
Ferney Chaparro <williamchaparro@unisangil.edu.co>
2022-10-11 21:12:17 -0500

    Update a.dart

--- funciones/a.dart
+++ funciones/a.dart
@@ -73,3 +73,62 @@ loop:while(true){
   print('######');
 }
 }
+//////
+
+import 'dart:io';
+int x=0,y=0;
+void menu(){ 
+  print('1. Asignar valores');
+  print('2. sumar');
+  print('3. restar');
+  print('4. multiplicar');
+  print('5. dividir');
+  print('6. modulo');
+  print('7. salir');
+}
+
+int asignar() => int.parse(stdin.readLineSync());
+
+int asignar2() {
+  print('ingrese un numero');
+  x = int.parse(stdin.readLineSync());
+  return x;
+}
+
+int asignar3() {    
+  print('ingrese un numero');
+  String t = stdin.readLineSync();  
+  y=int.parse(t);
+  return y; 
+}
+
+int sumar(a,b,c) => a+b+c;
+
+
+
+void main(){
+  String opcion="";
+  int a=0,b=0,c=0;
+loop:while(true){
+  menu();
+  print('ingrese una opcion');
+  opcion=stdin.readLineSync();
+  //opcionde de cada caso con un switch
+  switch(opcion){
+    case '1':
+      print('ingrese a');
+      a=asignar();
+      print('ingrese b');
+      b=asignar2();
+      print('ingrese c');
+      c=asignar3();
+      break;
+    case '2':
+      print('la suma es ${sumar(a,b,c)}');
+      break loop;    
+    case '7':
+      break loop;
+  }
+  print('######');
+}
+}
