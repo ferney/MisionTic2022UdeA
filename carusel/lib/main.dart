293218c8c94d11c594a8ec195a12bea62eb558fc
Ferney Chaparro <williamchaparro@unisangil.edu.co>
2022-10-17 18:42:44 -0500

    Add files via upload

---
+++ carusel/lib/main.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+/* He escogido la librería carousel_slider
+   La he elegido porque como he comentado en el ejercicio 4 cuando elegí el Animated Icon,
+   me parece que una aplicación con una parte visual llamativa y bonita tiene las de ganar
+   frente a una que no lo tenga. Al usuario las aplicaciones les entran por lo ojos lo primero, 
+   por lo que si no es "bonita" muchas veces la descartan directamente.
+   Por eso creo que para ver imagenes sobretodo, un carrusel es perfecto para mostrarlas de una forma
+   llamativa y "mas profesional"
+   
+   He realizado dos ejemplos el básico que muestra de forma mas facil como funciona esta librería y luego
+   uno con imágenes ya que era lo que quería aprender y lo que me parece más útil para esta librería*/
+
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+
+void main() => runApp(CarouselDemo());
+
+class CarouselDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(initialRoute: '/', routes: {
+      '/': (ctx) => CarouselDemoHome(),
+      '/basic': (ctx) => BasicDemo(),
+      '/complicated': (ctx) => ComplicatedImageDemo(),
+    });
+  }
+}
+
+class BasicDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List<int> list = [1, 2, 3, 4, 5];
+    return Scaffold(
+      appBar: AppBar(title: Text('Demo básico')),
+      body: Container(
+          child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(),
+              items: list
+                  .map((item) => Container(
+                        child: Center(child: Text(item.toString())),
+                        color: Colors.green,
+                      ))
+                  .toList(),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+class DemoItem extends StatelessWidget {
+  final String title;
+  final String route;
+  DemoItem(this.title, this.route);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      onTap: () {
+        Navigator.pushNamed(context, route);
+      },
+    );
+  }
+}
+
+class CarouselDemoHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Carousel de ejemplo'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            DemoItem('con numeros', '/basic'),
+            DemoItem('con imagenes', '/complicated'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          child: Container(
+            margin: EdgeInsets.all(5.0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                child: Stack(
+                  children: <Widget>[
+                    Image.network(
+                      item,
+                      fit: BoxFit.cover,
+                      width: 1000.0,
+                    ),
+                    Positioned(
+                      bottom: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(200, 0, 0, 0),
+                              Color.fromARGB(0, 0, 0, 0)
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        child: Text(
+                          'No. ${imgList.indexOf(item)} image',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ))
+    .toList();
+
+class ComplicatedImageDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('con imágenes')),
+      body: Container(
+          child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CarouselSlider(
+              options: CarouselOptions(
+                autoPlay: true,
+                aspectRatio: 2.0,
+                enlargeCenterPage: true,
+              ),
+              items: imageSliders,
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
