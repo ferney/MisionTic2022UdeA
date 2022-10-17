6da37a0b2b95d7f3d9e8e6431777a4280935734b
Ferney Chaparro <williamchaparro@unisangil.edu.co>
2022-10-17 18:01:04 -0500

    Add files via upload

---
+++ splash_simple/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+import 'dart:async';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FlutterLogo(
+          size: 400,
+        ),
+      ),
+    );
+  }
+}
