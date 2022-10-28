db657dce6eceb1d69174e7857c8d08ef6e4a86a3
Ferney Chaparro <williamchaparro@unisangil.edu.co>
2022-10-27 19:37:27 -0500

    Create modelo.g.dart

---
+++ Hive_2/modelo.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'modelo.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ItemAdapter extends TypeAdapter<Item> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Item read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Item(
+      name: fields[0] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Item obj) {
+    writer
+      ..writeByte(1)
+      ..writeByte(0)
+      ..write(obj.name);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ItemAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
