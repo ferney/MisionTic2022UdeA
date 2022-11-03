c81633ca78a8e8b9e4abf64cafcaf89268766881
Ferney Chaparro <williamchaparro@unisangil.edu.co>
2022-11-02 21:00:08 -0500

    Create mainRouter.dart

---
+++ navegacion/mainRouter.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //configurar rutas
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MyHomePage(),
+        '/Detalles': (context) => Detalles(),
+      },
+      title: 'Generated App',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF2196f3),
+        accentColor: const Color(0xFF2196f3),
+        canvasColor: const Color(0xFFfafafa),
+      ),
+      //home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: new Text('principal'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            child: const Text('abrir rurta'),
+            onPressed: () {
+              Navigator.pushNamed(context, '/Detalles');
+            },
+          ),
+        ));
+  }
+}
+
+class Detalles extends StatelessWidget {
+  Detalles({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text('secundaria'),
+      ),
+      body: Center(
+          child: ElevatedButton(
+              child: const Text('back'),
+              onPressed: () {
+                Navigator.pop(context);
+              })),
+    );
+  }
+}
