420017104dbdbb4141a8011eb89c7f2eb866e9b8
Ferney Chaparro <williamchaparro@unisangil.edu.co>
2022-11-06 16:49:44 -0500

    Update connection.php

--- LoginConMysql/connection.php
+++ LoginConMysql/connection.php
@@ -1,7 +1,12 @@
 <?php
-    
+
+    $host = "localhost";//HOSTING
+    $user = "id7264914_root"; //USUARIO
+    $pw = "90100100"; //PASWORD GENERADA EN 000WEBHOTS
+    $db = "id7264914_FLUTTER"; //BASE DE DATOS
+   
     // Variables de la conexion a la DB
-    $mysqli = new mysqli("localhost/url","usuario","contraseña","nombre de la base de datos");
+    $mysqli = new mysqli($host,$user,$pw,$db);
     
     // Comprobamos la conexion primeramente para saber si esta mal, el error primero
     if($mysqli->connect_errno) {
