de9bad80041512d0778385ab8d492c0e6e1396d3
Ferney Chaparro <williamchaparro@unisangil.edu.co>
2022-11-06 19:43:01 -0500

    Create main.dart

---
+++ sqflite/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'xyz',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: const HomePage(),
+    );
+  }
+}
