5edc2d5a03386f82f309844b12e4c681bde9509e
Ferney Chaparro <williamchaparro@unisangil.edu.co>
2022-11-07 15:32:01 -0500

    Create notificaciones.dart

---
+++ Notificaciones/notificaciones.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> initNotifications() async {
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('icono_notificacion');
+
+  const DarwinInitializationSettings initializationSettingsIOS =
+      DarwinInitializationSettings();
+
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    );
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+}
+
+Future<void> showNotificacion() async {
+  const AndroidNotificationDetails androidNotificationDetails =
+      AndroidNotificationDetails(
+    'your channel id',
+    'your channel name',
+    importance: Importance.max,
+    priority: Priority.high,
+  );
+
+  const NotificationDetails notificationDetails = NotificationDetails(
+    android: androidNotificationDetails,
+  );
+
+  await flutterLocalNotificationsPlugin.show(
+      1,
+      'Titulo de notificacion',
+      'Esta es una notificación de prueba para mostrar en el canal. Los quiero mucho.',
+      notificationDetails);
+}
