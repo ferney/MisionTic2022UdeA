3855b98f97038c6a37554fd1caf66eebc52a2c45
Ferney Chaparro <williamchaparro@unisangil.edu.co>
2022-11-09 21:58:17 -0500

    Add files via upload

---
+++ administrador apirest/js/geturl.js
@@ -0,0 +1,10 @@
+function getUrlVal() {
+	var val = [], hash;
+	var dir = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	for (var i = 0; i < dir.length; i++) {
+		hash = dir[i].split('=');
+		val.push(hash[0]);
+		val[hash[0]] = hash[1];
+	}
+	return val;
+}
